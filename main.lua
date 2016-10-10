5019ffb44bdb4d478ddbb2de45608fcb1acccd2a
Polly Nielsen Boutet-Livoff <pollyboutet@gmail.com>
2016-10-11 00:02:54 +0200

    add stuff i guess, i'll just redo everythin

--- main.lua
+++ main.lua
@@ -6,16 +6,25 @@ local state = "game"
 local State = {}
 
 function love.load()
-  if state == "game" then
-    Game:load(State)
-  end
+  Game:load(State)
+  Menu:load(State)
 end
 
 function love.update(dt)
   if state == "game" then
     Game:update(dt)
-  elseif state == "menu" then
-    Menu:update(dt)
+  end
+end
+
+function love.textinput(t)
+  if state == "menu" then
+    Menu:textinput(t)
+  end
+end
+
+function love.keypressed(key)
+  if state == "menu" then
+    Menu:keypressed(key)
   end
 end
 
@@ -27,7 +36,12 @@ function love.draw()
   end
 end
 
+function State:end_game (points)
+  State:set("menu")
+  Menu.points = points
+end
+
 function State:set(v)
   state = v
-  love.load()
+  Game:load(State)
 end
