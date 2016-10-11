bb4e6df19c73a21cbded476d1cbc7ad85452ff45
Polly Nielsen Boutet-Livoff <pollyboutet@gmail.com>
2016-10-11 17:41:38 +0200

    make death screen (this version deosn't owrk)

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 local player = require "player"
 local Agent = require "agent"
 
+playing = true
+
 function math.clamp(val, min, max)
   if val < min then
     return min
@@ -31,20 +33,29 @@ function love.load()
 end
 
 function love.update(dt)
-  player:update(dt, agents)
-  for i, v in ipairs(agents) do
+  if playing then
+    player:update(dt, agents)
     at = math.sqrt(player.velX^2 + player.velY^2) / (35E4 * dt)
-    v:update(at, player)
+    for i, v in ipairs(agents) do
+      v:update(at, player)
+    end
+  else
+
   end
 
   if player.dead then
-    love.load()
+    playing = false
   end
 end
 
 function love.draw()
-  player:draw()
-  for i, v in ipairs(agents) do
-    v:draw()
+  if playing then
+    player:draw()
+    for i, v in ipairs(agents) do
+      v:draw()
+    end
+  else
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.print(player.points, 100, 100)
   end
 end
