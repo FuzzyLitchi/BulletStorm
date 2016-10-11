845b5fea9caddf9d60be925627062785310ff828
Polly Nielsen Boutet-Livoff <pollyboutet@gmail.com>
2016-10-11 07:32:00 +0200

    recode some of it so it works better

--- main.lua
+++ main.lua
@@ -1,47 +1,50 @@
-local Game = require "game"
-local Menu = require "menu"
+local player = require "player"
+local Agent = require "agent"
 
-local state = "game"
-
-local State = {}
+function math.clamp(val, min, max)
+  if val < min then
+    return min
+  elseif val > max then
+    return max
+  else
+    return val
+  end
+end
 
 function love.load()
-  Game:load(State)
-  Menu:load(State)
+
+  agents = {}
+
+  player:load()
+
+  for i = 0, 10 do
+    local agent = Agent:make(math.random(0, love.graphics.getWidth()), 10)
+    table.insert(agents, agent)
+    agent:load(player)
+  end
+
+  for i = 0, 10 do
+    local agent = Agent:make(0, math.random(0, love.graphics.getHeight()) - 26)
+    table.insert(agents, agent)
+    agent:load(player)
+  end
 end
 
 function love.update(dt)
-  if state == "game" then
-    Game:update(dt)
+  player:update(dt, agents)
+  for i, v in ipairs(agents) do
+    at = math.sqrt(player.velX^2 + player.velY^2) / (35E4 * dt)
+    v:update(at, player)
   end
-end
 
-function love.textinput(t)
-  if state == "menu" then
-    Menu:textinput(t)
-  end
-end
-
-function love.keypressed(key)
-  if state == "menu" then
-    Menu:keypressed(key)
+  if player.dead then
+    love.load()
   end
 end
 
 function love.draw()
-  if state == "game" then
-    Game:draw()
-  elseif state == "menu" then
-    Menu:draw()
+  player:draw()
+  for i, v in ipairs(agents) do
+    v:draw()
   end
-end
-
-function State:end_game (points)
-  State:set("menu")
-  Menu.points = points
-end
-
-function State:set(v)
-  state = v
-  Game:load(State)
 end
